8a94dded6abdd8fa7ca1d657d52b251389f20150
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-01-20 11:26:27 +0200

    first commit

---
+++ ml.py
@@ -0,0 +1,45 @@
+# Numpy is a python library.
+
+#Numpy is used for working with arrays.
+
+#Numpy is short for "Numerical Python"
+
+
+
+
+
+# import numpy as b
+
+
+
+# a=b.array([1,2,3,4,5])
+
+
+# print(a)
+
+
+
+# import numpy as np
+
+
+# arr=np.array(42)
+
+
+
+# print(arr)
+
+
+#create a 2-d array containing two arrays with the values 1,2,3 and 4,,5,6
+
+import numpy as np
+
+
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr)
+
+
+
+
+#the result will be in 2-d array
+
+
